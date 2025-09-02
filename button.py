91518024bed818dc5f5e6d9e8a32d5735e2cae5b
Lenpka <leanidkonstantinov@yandex.ru>
2025-09-02 09:41:56 +0300

    row button

--- button.py
+++ button.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
-from aiogram.utils.keyboard import KeyboardBuilder
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 import asyncio
 from aiogram.types import (
     Message,
@@ -15,15 +15,17 @@ BOT_TOKEN = env("BOT_TOKEN")
 bot = Bot(BOT_TOKEN)
 disp = Dispatcher()
 # Кнопки и клавиатура
-Button1 = KeyboardButton(text = 'Кнопка 1')
-Button2 = KeyboardButton(text = 'Кнопка 2')
+# Button1 = KeyboardButton(text = 'Кнопка 1')
+# Button2 = KeyboardButton(text = 'Кнопка 2')
+keyboard = ReplyKeyboardBuilder()
 array_of_button = [KeyboardButton( text = f"Button{j*3 + i}") for i in range(1, 4) for j in range(3)]
-Keyboard = ReplyKeyboardMarkup(keyboard=[array_of_button], resize_keyboard= True, one_time_keyboard= True)
+#Keyboard = ReplyKeyboardMarkup(keyboard=[array_of_button], resize_keyboard= True, one_time_keyboard= True)
+keyboard.row(*array_of_button, width = 3)
 
 @disp.message(CommandStart())
 async def start(message:Message):
-    await message.answer(text = "Здарова, смотри какие кнопки внизу, давай глянем?",
-                         reply_markup=Keyboard)
+    await message.answer(text = "Здарова, смотри какая клава?",
+                         reply_markup=keyboard.as_markup())
     # Отправка клавиатуры
 
 
